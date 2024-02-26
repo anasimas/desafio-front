752d3cb0b00da9187ac451ea18edd85f9a8a7f60
Ana Simas <86128673+anasimas@users.noreply.github.com>
2024-02-26 14:20:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function apagarTudo(){
+    document.getElementById('resultado').innerHTML = ""
+}
+
+function limpar(){
+    let calculo = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = calculo.substring(0, calculo.length-1);
+}
+
+function inserir(dado){
+    let calculo = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = calculo + dado;
+}
+
+function calcular(){
+    let calculo = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = eval(calculo);
+}
