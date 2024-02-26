fad1c4936f05b1e63b61ca71c12d493d7e3b6aaa
Ana Simas <86128673+anasimas@users.noreply.github.com>
2024-02-26 14:41:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ function calcular(){
     let calculo = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = eval(calculo);
 }
+
+function power(exp){
+    let calculo = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = calculo + `^${exp}`;
+    document.getElementById('resultado').innerHTML = Math.pow(calculo,exp);
+}
